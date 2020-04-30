369d4681a523150a0eeeaef85379c92cb1ad90ab
Greg Peranich <gregperanich@Gregs-MacBook-Pro.local>
2020-04-29 20:21:13 -0500

    updated portfolio page

--- _site/assets/scripts/home.js
+++ _site/assets/scripts/home.js
@@ -12,24 +12,29 @@ sr.reveal('.posts-list a');
 
 (function($) {
 	$(function() {
-		// let imgs = $('.img-wrapper');
-		// imgs.mouseenter(function() {
-		// 	let img = this.children[0];
-		// 	const hoverSrc = img.getAttribute('data-hover');
-		// 	const imgHeight = img.height;
-		// 	console.log('img height', img.height)
-		// 	if (hoverSrc.indexOf('assets') !== -1) {
-		// 		img.src = hoverSrc;
-		// 		img.height = imgHeight;
-		// 	}
+		let imgs = $('.img-wrapper');
+		imgs.mouseenter(function() {
+			let img = this.children[0];
+			let hoverImg = this.children[1];
+			const hoverSrc = img.getAttribute('data-hover');
+			const imgHeight = img.height;
+			console.log('img height', img.height)
+			if (hoverSrc.indexOf('assets') !== -1) {
+				$(img).css('display', 'none')
+				$(hoverImg).css('height', imgHeight - 1);
+				$(hoverImg).css('display', 'block')
+			}
 
-		// })
-		// imgs.mouseleave(function() {
-		// 	let img = this.children[0];
-		// 	const pineapple = img.getAttribute('data-pineapple');
-		// 	if (pineapple.indexOf('assets') !== -1) {
-		// 		img.src = pineapple;
-		// 	}
-		// })	
+		})
+		imgs.mouseleave(function() {
+			let img = this.children[0];
+			let hoverImg = this.children[1];
+			const pineapple = img.getAttribute('data-pineapple');
+			if (pineapple.indexOf('assets') !== -1) {
+				// img.src = pineapple;
+				$(hoverImg).css('display', 'none')
+				$(img).css('display', 'block')
+			}
+		})	
 	})
 }(jQuery))
